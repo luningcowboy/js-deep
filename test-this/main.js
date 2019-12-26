2e2daf23ed7ae338e2751f45c49cc47dd111cf66
luningcowboy <luningcowboy@gmail.com>
2019-12-26 14:55:44 +0800

    new code

--- test-this/main.js
+++ test-this/main.js
@@ -23,3 +23,32 @@ o.foo(); // 3 this === o
 // 我的猜想:
 // node运行的时候，自己给作用域不明确的代码创建了一个临时作用域，这部分内容会绑定
 // 到临时作用域而不是全局对象上
+// 上面的原因是node的代码默认应该是采用的严格模式，直接将作用域绑定到了undefined
+
+if(!Function.prototype.softBind){
+    Function.prototype.softBind = obj=>{
+        let fn = this;
+        let curried = [].slice.call(arguments, 1);
+        let bound = ()=>{
+            return f.apply((!this || this === root )? obj : this, curried.concat.apply(curried, arguments));
+        };
+        bound.prototype = Object.create(fn.prototype);
+        return bound;
+    };
+}
+function foo1(){
+    console.log("name:", this.name);
+}
+let obj = {name: 'o1'},
+    obj2 = {name: 'o2'},
+    obj3 = {name: 'o3'};
+let fooOBJ = foo1.softBind(obj);
+fooOBJ();
+obj2.foo = foo1.softBind(obj);
+obj2.foo();
+
+fooOBJ.call(obj3);
+setTimeout(obj2.foo, 10);
+
+
+
